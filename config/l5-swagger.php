26cb942afd7a2724563ce6a89f42f4a0d8a0d9f8
hui <huynguyeexn@gmail.com>
2021-08-30 20:13:05 +0700

    Change API docs to => api/docs

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -5,14 +5,14 @@ return [
     'documentations' => [
         'default' => [
             'api' => [
-                'title' => 'L5 Swagger UI',
+                'title' => 'CINEBEE API - L5 Swagger UI',
             ],
 
             'routes' => [
                 /*
                  * Route for accessing api documentation interface
                 */
-                'api' => 'api/documentation',
+                'api' => 'api/docs',
             ],
             'paths' => [
                 /*

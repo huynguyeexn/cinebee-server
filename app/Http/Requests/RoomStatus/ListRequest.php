a54829200b087a2cb826e100150d8f49a640c026
hui <huynguyeexn@gmail.com>
2021-09-02 14:35:45 +0700

    Example API Docs for RoomStatus

---
+++ app/Http/Requests/RoomStatus/ListRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\RoomStatus;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ListRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            'q' => 'nullable|string|',
+            'page' => 'nullable|numeric',
+            'per_page' => 'nullable|numeric|min:0',
+            'sort_by' => 'nullable|string',
+            'sort_type' => [
+                'nullable',
+                Rule::in(['asc', 'desc']),
+            ],
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SeatStatusSeed::class,
             UserRoleSeed::class,
-            UserSeed::class
+            UserSeed::class,
+            RoomStatusSeed::class
         ]);
     }
 }

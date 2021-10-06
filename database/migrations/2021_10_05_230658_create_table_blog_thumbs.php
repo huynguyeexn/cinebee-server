937b6d6691298c89da0b0537c606f9b61a8a343c
ThuyDung2001 <dunglttps14010@fpt.edu.vn>
2021-10-06 11:36:32 +0700

    blogthumb

---
+++ database/migrations/2021_10_05_230658_create_table_blog_thumbs.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableBlogThumbs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_thumbs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('blog_id')
+                ->constrained()
+                ->cascadeOnUpdate();
+            $table->foreignId('file_upload_id')
+                ->constrained()
+                ->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_thumbs');
+    }
+}
